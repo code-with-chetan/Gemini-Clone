fb7bb60db4e3f53708e33702a05e2aa0f18c0c84
chetan rana <ranachetan455@gmail.com>
2025-09-27 16:21:30 +0545

    resonsive and word scroll featured added

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,27 +1,44 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { assets } from "../../assets/assets";
+import { Context } from "../../context/Context";
 
 const Sidebar = () => {
 
     const [extended, setExtended] = useState(false);
+    const{onSent,prevPrompt,setRecentPrompt,newChat}=useContext(Context);
+
+    const loadPrompt=async(prompt)=>{
+        setRecentPrompt(prompt);
+        await onSent(prompt);
+    }
 
     return (
-        <div className="flex flex-col justify-between min-h-screen bg-[#f0f4f9] px-[20px] py-[15px]">
+        <div className="flex flex-col justify-between min-h-screen bg-[#f0f4f9] px-[20px] py-[15px]
+        max-[600px]:hidden">
             <div className="top">
                 <img onClick={() => setExtended(prev => !prev)} src={assets.menu_icon} alt="menu icon"
                     className="block ml-[10px] cursor-pointer" />
-                <div className="mt-[50px] inline-flex items-center gap-[10px] px-[10px] py-[10px]
+                <div onClick={()=>newChat()} className="mt-[50px] inline-flex items-center gap-[10px] px-[10px] py-[10px]
                 bg-[#e6eaf1] rounded-full text-[14px] text-gray-600 cursor-pointer">
                     <img src={assets.plus_icon} alt="plus icon" />
                     {extended ? <p>New Chat</p> : null}
                 </div>
                 {extended ?
-                    <div className="flex flex-col">
+                    <div className="flex flex-col animate-[fadeIn_1s_ease-in-out]">
                         <p className="mt-[30px] mb-[20px]">Recent</p>
-                        <div className="recent-entry">
-                            <img src={assets.message_icon} alt="message icon" />
-                            <p>What is React</p>
-                        </div>
+                        {
+                            prevPrompt.map((items,index)=>{
+                                return(
+
+                                <div onClick={()=>loadPrompt(items)} className="recent-entry" key={index}>
+                                     <img src={assets.message_icon} alt="message icon" />
+                                      <p>{items.slice(0,18)} ...</p>
+                                    </div>
+                                    
+                                )
+                            })
+                        }
+                       
                     </div>
                     : null}
 
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,67 +1,76 @@
 import { createContext, useState } from "react";
 import runChat from "../config/gemini";
 
-export const Context=createContext();
+export const Context = createContext();
 
-const ContextProvider=(props)=>{
+const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPrompt, setPrevPrompt] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
 
-    const[input,setInput]=useState("");
-    const[recentPrompt,setRecentPrompt]=useState("");
-    const[prevPrompt,setPrevPrompt]=useState([]);
-    const[showResult,setShowResult]=useState(false);
-    const[loading,setLoading]=useState(false);
-    const[resultData,setResultData]=useState("");
+  const delayPara = (index, nextWord) => {
+    setTimeout(() => {
+      setResultData((prev) => prev + nextWord);
+    }, 75 * index); // adjust typing speed here
+  };
 
-    const delayPara=(index,nextWord)=>{
 
+  const newChat=()=>{
+    setLoading(false);
+    setShowResult(false);
+  }
+
+  const onSent = async (prompt) => {
+    setResultData("");
+    setLoading(true);
+    setShowResult(true);
+    
+    let response;
+    if(prompt !== undefined){
+      response=await runChat(prompt);
+      setRecentPrompt(prompt);
+    }
+    else{
+         setPrevPrompt(prev=>[...prev,input])
+         setRecentPrompt(input);
+         response=await runChat(input);
+    }
+    
+    let formatted = response.replace(/\*\*(.*?)\*\*/g, "<b>$1</b>");
+    formatted = formatted.replace(/(\n\s*\n|\n|\*)/g, "<br/>");
+    formatted = formatted.replace(/\*/g, "");
+    let newResponse = formatted.split(" ");
+    for (let i = 0; i < newResponse.length; i++) {
+      const nextWord = newResponse[i];
+      delayPara(i, nextWord + " ");
     }
 
-    const onSent=async (prompt)=>{
+    setLoading(false);
+    setInput("");
+  };
 
-            setResultData("");
-            setLoading(true);
-            setShowResult(true);
-            setRecentPrompt(input);
+  const contextValue = {
+    prevPrompt,
+    setPrevPrompt,
+    onSent,
+    setRecentPrompt,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    input,
+    setInput,
+    newChat,
+  };
 
-           const response = await runChat(input);
-           let responseArray=response.split("**");
-           let newResponse="";
-           for(let i=0;i<responseArray.length;i++){
-                if(i===0 || i%2!==1){
-                    newResponse += responseArray[i];
-                }
-                else{
-                    newResponse += "<b>"+responseArray[i]+"</b>";
-                }
-           }
-           let newResponse2=newResponse.split("*").join("<br/>");
-           setResultData(newResponse2);
-           setLoading(false);
-           setInput("");
-    }
-
-   
-
-
-    const contextValue={
-      
-        prevPrompt,
-        setPrevPrompt,
-        onSent,
-        setRecentPrompt,
-        recentPrompt,
-        showResult,
-        loading,
-        resultData,
-        input,
-        setInput
-    }
-
-    return(
-        <Context.Provider value={contextValue}>
-            {props.children}
-        </Context.Provider>
-    )
-}
+  return (
+    <Context.Provider value={contextValue}>
+      {props.children}
+    </Context.Provider>
+  );
+};
 
 export default ContextProvider;
